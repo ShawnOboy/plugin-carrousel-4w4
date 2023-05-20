74e8730417fe31e3da60df6e7864d944607ce972
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-05-19 21:01:21 -0400

    redimension du carrousel automatiquement lorsque la fenetre change

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
   // Variables
   let bouton = document.querySelector(".carrousel__ouvrir")
   let carrousel  = document.querySelector(".carrousel")
@@ -15,18 +15,28 @@
   let ancien_index = -1
 
   // Événements
-  carrousel__x.addEventListener('mousedown', function(){
+  carrousel__x.addEventListener('mousedown', function() {
     carrousel.classList.remove('carrousel--activer')
     for (const article of articles) {
       article.style.pointerEvents = "all";
     }
+    clearInterval(redimension_Carrousel)
+  })
+
+  flechG.addEventListener("mousedown", function() {
+    index = index > 0 ? index - 1 : galerie__img.length - 1
+    afficher_image(index)
+  })
+
+  flechD.addEventListener("mousedown", function() {
+    index = index < galerie__img.length - 1 ? index + 1 : 0
+    afficher_image(index)
   })
 
   // Fonctions
-  for (const elm of galerie__img)
-  {
+  for (const elm of galerie__img) {
     elm.dataset.index = position
-    elm.addEventListener('mousedown', function(evt){
+    elm.addEventListener('mousedown', function(evt) {
       carrousel.classList.add('carrousel--activer')
       index = evt.target.dataset.index
       afficher_image(index)
@@ -42,7 +52,7 @@
     rad.setAttribute('name', 'carrousel__rad')
     rad.classList.add('carrousel__rad')
     rad.dataset.index = position
-    rad.addEventListener('mousedown', function(){
+    rad.addEventListener('mousedown', function() {
       index = this.dataset.index
       afficher_image(index)
     })
@@ -50,18 +60,8 @@
     carrousel__form.appendChild(rad)
   }
 
-  flechG.addEventListener("mousedown", function() {
-    index = index > 0 ? index - 1 : galerie__img.length - 1
-    afficher_image(index)
-  })
-
-  flechD.addEventListener("mousedown", function() {
-    index = index < galerie__img.length - 1 ? index + 1 : 0
-    afficher_image(index)
-  })
-
-  function afficher_image(index){
-    if (ancien_index != -1){
+  function afficher_image(index) {
+    if (ancien_index != -1) {
       carrousel__figure.children[ancien_index].classList.remove('carrousel__img--activer')
     }
     carrousel__figure.children[index].classList.add('carrousel__img--activer')
@@ -69,6 +69,7 @@
       article.style.pointerEvents = "none";
     }
     ancien_index = index
+    // redimension_Carrousel()
     verif_index()
   }
 
@@ -77,5 +78,28 @@
     for (let i = 0; i < btnsRad.length; i++) {
       btnsRad[i].checked = i == index
     }
+    clearInterval(redimension_Carrousel)
+    setInterval(redimension_Carrousel, 100)
   }
+
+  function redimension_Carrousel(){
+    const imageWidth = carrousel__figure.children[index].naturalWidth
+    const imageHeight = carrousel__figure.children[index].naturalHeight
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+  
+    let carrouselHeight = windowHeight 
+    if (windowWidth > 1000) {
+      carrouselHeight = windowHeight - windowHeight/3
+    }
+  
+    let carrouselWidth = windowWidth - windowWidth/3 //carrouselHeight * imageWidth/imageHeight
+  
+    carrousel.style.width = `${carrouselWidth}px`
+    carrousel.style.height = `${carrouselHeight}px`
+    carrousel.style.top= `${-(windowHeight-carrouselHeight)/2}px`
+    carrousel.style.left= `${0}px`
+
+  }
+  
 })()
